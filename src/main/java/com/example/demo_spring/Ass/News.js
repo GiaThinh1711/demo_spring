0b76a0851c14b76771db44d43eca70c2ac158d8a
GiaThinh1711 <thinhdau1711@gmail.com>
2022-04-22 17:42:54 +0700

    asdasdasd

--- src/main/java/com/example/demo_spring/Ass/News.js
+++ src/main/java/com/example/demo_spring/Ass/News.js
@@ -6,21 +6,20 @@ document.addEventListener('DOMContentLoaded', function () {
             var table = document.querySelector('#news-table');
             for (var i = 0; i < data.length; i++) {
                 const element = data[i];
-                console.log(element)
                 table.innerHTML +=
                     `<tr>
                 <td>${element.id}</td>
                 <td>${element.title}</td>
                 <td>${element.description}</td>
-                <td>${element.image}</td>
+                <td><img style="height: 100px" src="${element.image}"></td>
                 <td>${element.content}</td>
                 <td>${element.category}</td>
                 <td>${element.createAt}</td>
                 <td>${element.updateAt}</td>
                 <td>${element.status}</td>
-                <td><a href="/HTML/Detail.html?id=${element.id}"><i class="fas fa-info-square"></i></a></td>
-                <td><i class="fas fa-edit" onclick='editproduct(${element.id})'></i></td>
-                <td><a href="/HTML/Detail.html?id=${element.id}"><i class="fas fa-trash" onclick='Delete(${element.id})'></i></a></td>
+                <td><a href="Detail.html?id=${element.id}"><i class="fas fa-info-square" onclick="loadNewsDetail(${element.id})"></i></a></td>
+                <td><a><i class="fas fa-edit" onclick='editproduct(${element.id})'></i></a></td>
+                <td><i class="fas fa-trash" onclick='deleteProduct(${element.id})'></i></td>
               </tr>`
             }
         }
@@ -28,3 +27,42 @@ document.addEventListener('DOMContentLoaded', function () {
     xhr.open('GET', 'http://localhost:8080/api/v1/news', false);
     xhr.send();
 });
+function deleteProduct(id){
+    if(id === undefined || id === null){
+        return;
+    }
+    if (confirm("Are you sure ?")){
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if(xhr.readyState === 4 && xhr.status === 200){
+                alert("Success");
+                window.location.href = '/demo_spring/com/example/demo_spring/Ass/Form_News.html';
+            }
+        };
+        xhr.open('DELETE','http://localhost:8080/api/v1/news/' + id,false);
+        xhr.send();
+    }
+}
+function loadNewsDetail(id) {
+    var getArticleDetailUrl = `http://localhost:8080/api/v1/news/${id}`;
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4){
+            if (xhr.status === 200){
+                var newsDetail = JSON.parse(xhr.responseText);
+                document.forms['news-table']['title'].value = newsDetail.title;
+                document.forms['news-table']['description'].value = newsDetail.description;
+                document.forms['news-table']['image'].value = newsDetail.image;
+                document.forms['news-table']['content'].value = newsDetail.content;
+                document.forms['news-table']['createAt'].value = newsDetail.createAt;
+                document.forms['news-table']['updateAt'].value = newsDetail.updateAt;
+                document.forms['news-table']['category'].value = newsDetail.category;
+                document.forms['news-table']['status'].value = newsDetail.status;
+            } else {
+                alert('Khong the load tin tuc');
+            }
+        }
+    }
+    xhr.open('GET' ,getArticleDetailUrl, false);
+    xhr.send();
+}
