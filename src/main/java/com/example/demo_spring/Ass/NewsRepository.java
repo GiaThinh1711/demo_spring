1d0bf4dc98b83d35745f1b880f99e6b9e3576941
GiaThinh1711 <thinhdau1711@gmail.com>
2022-04-23 00:15:22 +0700

    ád

--- src/main/java/com/example/demo_spring/Ass/NewsRepository.java
+++ src/main/java/com/example/demo_spring/Ass/NewsRepository.java
@@ -1,8 +1,16 @@
 package com.example.demo_spring.Ass;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NewsRepository extends JpaRepository<News,Integer> {
+    @Query(value = "SELECT * FROM news a WHERE a.title LIKE %:title% and a.category LIKE %:category%",
+            nativeQuery = true)
+    List<News> search(
+            @Param("title") String title, @Param("category") String category);
 }
